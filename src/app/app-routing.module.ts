dd46d5ae2dd99e5aead5d04e984fafeb670ea8c2
LucasHxC <lucashxc@gmail.com>
2023-07-11 23:55:54 +0100

    resp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomePage } from './home/home.page';
 import { NewPostPage } from './new-post/new-post.page';
 import { PostListPage } from './post-list/post-list.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'post-list', pathMatch: 'full' },
-  { path: 'post-list', component: PostListPage },
-  { path: 'new-post', component: NewPostPage },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'new-post',
+    component: NewPostPage
+  },
+  {
+    path: 'post-list',
+    component: PostListPage
+  },
+  // Add more routes for other components/pages here
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,16 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-}
+    goToPostListPage() {
+      this.router.navigate(['/post-list']);
+    }
+  
+    goToNewPostPage() {
+      this.router.navigate(['/new-post']);
+    }
+  }
